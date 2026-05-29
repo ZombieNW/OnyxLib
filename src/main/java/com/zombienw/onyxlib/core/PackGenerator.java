a38c14a8ccafe00381c868937a6b675d383714a8
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2026-05-29 13:34:25 -0700

    Create PackGenerator.java

---
+++ src/main/java/com/zombienw/onyxlib/core/PackGenerator.java
@@ -0,0 +1,9 @@
+package com.zombienw.onyxlib.core;
+
+/**
+ * The resource-pack generator for all OnyxLib plugins.
+ */
+public class PackGenerator {
+
+    private static final int PACK_FORMAT = 42;
+}
