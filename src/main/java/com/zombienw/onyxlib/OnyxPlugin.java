34259c3ca0c5864f30988895f43819abfe90c20a
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2026-06-27 15:25:10 -0700

    Better error handle JavaPlugin

--- src/main/java/com/zombienw/onyxlib/OnyxPlugin.java
+++ src/main/java/com/zombienw/onyxlib/OnyxPlugin.java
@@ -5,6 +5,7 @@ import com.zombienw.onyxlib.impl.registry.NamespaceRegistry;
 import org.bukkit.command.Command;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerLoadEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -21,25 +22,30 @@ public class OnyxPlugin extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(new OnyxBlockListener(this), this);
 
         // Register command
-        OnyxCommand command = new OnyxCommand(this);
         PluginCommand pluginCommand = getCommand("onyx");
-        if (pluginCommand == null) throw new RuntimeException("OnyxLib command 'onyx' not found.");
+        if (pluginCommand == null) {
+            getLogger().severe("Command '/onyx' is missing from plugin.yml");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        OnyxCommand command = new OnyxCommand(this);
         pluginCommand.setExecutor(command);
         pluginCommand.setTabCompleter(command);
-
 
         getLogger().info("OnyxLib has been initialized.");
     }
 
     @Override
     public void onDisable() {
-        // prevent memory leak on reload
+        HandlerList.unregisterAll((JavaPlugin) this);
         NamespaceRegistry.clear();
+        getLogger().info("OnyxLib namespaces cleared and plugin disabled.");
     }
 
     @EventHandler
     public void onServerLoad(ServerLoadEvent event) {
-        getLogger().info("Server loaded. Locking OnyxLib namespaces.");
+        getLogger().info("Server startup complete. Locking OnyxLib namespaces.");
         NamespaceRegistry.lockAll();
     }
 }
